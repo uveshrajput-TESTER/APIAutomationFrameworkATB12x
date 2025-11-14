e470a610625ba35de0879b7d7b7b2cb9863d3e59
uveshrajput-TESTER <uveshrajpute@gmail.com>
2025-11-14 15:18:30 +0530

    nov 14

---
+++ src/main/java/org/thetestingAcademy/App.java
@@ -0,0 +1,13 @@
+package org.thetestingAcademy;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
